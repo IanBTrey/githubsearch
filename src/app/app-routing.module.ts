c629ad8a5c72c1307f1ddb2bc845f483037ff3bc
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-06 11:01:11 +0300

    added LICENSE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "landing", component: LandingComponent}
+  ,{path: "repositories", component: RepositoriesComponent}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/highlighter.directive.ts
@@ -0,0 +1,16 @@
+// import { Directive,ElementRef,HostListener } from '@angular/core';
+// import { User } from './user'
+// import { LandingComponent } from '.landing/landing.component'
+//
+// @Directive({
+//   selector: '[appHighlighter]'
+// })
+// export class HighlighterDirective {
+//
+//   @HostListener("input") input()
+//   user: User;
+//
+//     constructor(private elem: ElementRef) {
+//       this.elem.nativeElement.style.backgroundColor = 'red';
+//     }
+//   }
