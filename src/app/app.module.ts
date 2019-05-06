3451d27370014dfc3819cefde4b9d441b6916bcc
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-06 10:37:53 +0300

    added and modified imports and exports in my module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { LandingComponent } from './landing/landing.component';
+// import { HighlighterDirective } from './highlighter.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { FormsModule } from '@angular/forms';
+import { Repos } from './repos';
+import { RepositoryService } from './repository-http/repository.service';
+import { UserService } from './user-http/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoriesComponent,
+    LandingComponent,
+    // HighlighterDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [UserService, RepositoryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
